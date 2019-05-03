6e6b639527c77dc1ccf1e0791184be304a397bab
Friedrich Delgado <delgado@dfn-cert.de>
2019-05-03 13:03:14 +0200

    correct parameter passing

--- proj/proj/tests/test_views.py
+++ proj/proj/tests/test_views.py
@@ -4,12 +4,12 @@ pytestmark = [pytest.mark.django_db]
 
 
 def test_view(client):
-    response = client.get('/', args=[2, 3])
+    response = client.get('/', {'args[]': [2, 3]})
     assert response.status_code == 200
     assert 'The answer is 5.' in response.content.decode('utf-8')
-    response = client.get('/', args=[5, 4])
+    response = client.get('/', {'args[]': [5, 4]})
     assert response.status_code == 200
     assert 'The answer is 9.' in response.content.decode('utf-8')
-    response = client.get('/', args=[3, 5, 7])
+    response = client.get('/', {'args[]': [3, 5, 7]})
     assert response.status_code == 200
     assert 'The answer is 15.' in response.content.decode('utf-8')
