1e1425f6514c593d4961aee756486ac6298993a9
Friedrich Delgado <delgado@dfn-cert.de>
2019-05-03 12:49:09 +0200

    add basic test for view content

--- proj/proj/tests/test_views.py
+++ proj/proj/tests/test_views.py
@@ -4,5 +4,6 @@ pytestmark = [pytest.mark.django_db]
 
 
 def test_view(client):
-    response = client.get('/')
+    response = client.get('/', args=[2, 3])
     assert response.status_code == 200
+    assert 'The answer is 5.' in response.content.decode('utf-8')
