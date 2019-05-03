8274b977457f0427a262792349c4bbe95b25adab
Friedrich Delgado <delgado@dfn-cert.de>
2019-05-03 12:58:11 +0200

    add test for more parameters

--- proj/proj/tests/test_views.py
+++ proj/proj/tests/test_views.py
@@ -7,3 +7,9 @@ def test_view(client):
     response = client.get('/', args=[2, 3])
     assert response.status_code == 200
     assert 'The answer is 5.' in response.content.decode('utf-8')
+    response = client.get('/', args=[5, 4])
+    assert response.status_code == 200
+    assert 'The answer is 9.' in response.content.decode('utf-8')
+    response = client.get('/', args=[3, 5, 7])
+    assert response.status_code == 200
+    assert 'The answer is 15.' in response.content.decode('utf-8')
